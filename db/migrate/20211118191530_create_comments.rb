ca26a4445504c84a06c15f9176b83735ddfce5ba
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-11-18 12:33:28 -0700

    Clean up migration language

--- db/migrate/20211118191530_create_comments.rb
+++ db/migrate/20211118191530_create_comments.rb
@@ -2,12 +2,10 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.string :content
-      t.bigint  :parent_id
-      t.string  :parent_type
+      t.references :parent, polymorphic: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
-
-    add_index :comments, [:parent_type, :parent_id]
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   describe 'Comment relationships' do
     it { should belong_to(:user) }
-    it { should belong_to(:comments) }
+    it { should belong_to(:parent) }
   end
 end
