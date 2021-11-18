ef2faf7b1c4305ac7bb0c317f22b3ef4713ee88c
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-11-18 12:22:14 -0700

    Write relationship tests

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'Comment relationships' do
+    it { should belong_to(:user) }
+    it { should belong_to(:comments) }
+  end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Post relationships' do
+    it { should belong_to(:user) }
+    it { should have_many(:comments) }
+  end
 end
