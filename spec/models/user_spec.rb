39ded242a5751b054547b25966bdfb7bf6141ed9
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-11-18 12:35:27 -0700

    Add secure password test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_secure_password }
+
   describe 'User relationships' do
     it { should have_many(:posts) }
     it { should have_many(:comments) }
