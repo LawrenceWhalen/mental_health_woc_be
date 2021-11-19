56f20ebb33d01e3c15e301f5a4219d7a2d88a9dd
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-11-19 10:47:36 -0700

    Implement jwt denylist

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable, :registerable, :recoverable, :rememberable and :omniauthable
-  devise :database_authenticatable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable, :rememberable and :omniauthable
+  devise :database_authenticatable, :validatable, :registerable, 
+    :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   has_many :posts
   has_many :comments
